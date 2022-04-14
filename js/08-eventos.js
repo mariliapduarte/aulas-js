06d9021da0d11b29063344810ec91ccc9b1b227d
mariliapduarte <mariliapduarte@gmail.com>
2022-04-14 16:04:50 -0300

    formulario

--- js/08-eventos.js
+++ js/08-eventos.js
@@ -38,4 +38,46 @@ const campoNota2 = document.querySelector("#nota2");
 const divResultados = document.querySelector("#resultados");
 
 //Detextar o acionamento do formulário
-formulario.addEventListener("submit", function(){});
+// Obs. a função dentro do listener, é chamada de Callback
+formulario.addEventListener("submit", function(event){ 
+    event.preventDefault();//aqui anulamos o comportamento padrão do navegador de redirecionar o submit do formulário. Nós quem programamos isso
+
+    // 1) Capturar os dados digitados nos campos
+    let nome = campoNome.value;
+    let nota1 = parseFloat (campoNota1.value);
+    let nota2 = parseFloat (campoNota2.value);
+    
+    // 2) Calcular a média das notas e criar variável situação
+    let media = (nota1 + nota2) / 2;
+    let situacao; 
+    if( media >=7 ){
+        situacao = "Aprovado";
+    
+    } else {
+         situacao = "Reprovada";
+    }
+
+    // 3) Criando elemento dinamicamentevia JS
+
+    // 3.1) Criar o elemento/tag
+    let paragrafo = document.createElement("p")
+
+    // 3.2) Montar o conteúdo da tag
+    paragrafo.innerHTML = `<b>${nome}</b> - <i>${media}</i> - <b>${situacao}</b>`;
+
+    // 3.3) Adicionar tag/conteúdo à div de resultados
+    divResultados.appendChild(paragrafo);
+
+    formulario.reset(); //para resetar o formulário depois de enviar
+
+    //campoNome.value=""; - seria utilizado para cada campo
+
+    // == comparação - = atribuição
+
+    
+});
+
+
+
+
+  
